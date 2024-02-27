efc3c221264132e0279617ae5c5d0b1a448b0dd5
01Pratham <79049239+01Pratham@users.noreply.github.com>
2024-02-27 22:47:57 +0530

    Commit

--- estimate/add_product.php
+++ estimate/add_product.php
@@ -8,20 +8,22 @@ if ($_POST["request"] === "prod") {
     // echo "SELECT * FROM `product_list` WHERE `default_int` = '{$prod}'";
     elem();
 } else {
-?>
+    ?>
     <div id="<?= $category . "_{$id}" ?>">
         <div class="contain-btn btn-link border-bottom" id='vmHead_<?= $id ?>'>
-            <a class="btn btn-link text-left" id="vmHead_<?= $id ?>" data-toggle="collapse" href="#vm_collapse_<?= $id ?>" role="button" aria-expanded="true" aria-controls="vm_collapse_<?= $id ?>">
+            <a class="btn btn-link text-left" id="vmHead_<?= $id ?>" data-toggle="collapse" href="#vm_collapse_<?= $id ?>"
+                role="button" aria-expanded="true" aria-controls="vm_collapse_<?= $id ?>">
                 <i class="fa fa-desktop"></i>
                 <h6 class="d-inline-block ml-1">
                     <?= ucwords($category) ?> Services :
                 </h6>
                 <h6 class="d-inline-block ml-1 OnInput"></h6>
             </a>
-            <input type="button" value=" Remove " class="add-estmt btn btn-link float-right except remove" id="rem-vm_<?= $id ?>" data-toggle="button" aria-pressed="flase" autocomplete="on">
+            <input type="button" value=" Remove " class="add-estmt btn btn-link float-right except remove"
+                id="rem-vm_<?= $id ?>" data-toggle="button" aria-pressed="flase" autocomplete="on">
         </div>
         <div class="collapse show py-1" id="<?= $category . "collapse_{$id}" ?>">
-            <div class="row">
+            <div class="row main-row">
                 <?php elem() ?>
             </div>
         </div>
@@ -30,58 +32,69 @@ if ($_POST["request"] === "prod") {
     <script src="../javascript/main.js"></script>
     <script src="../javascript/jquery-3.6.4.js"></script>
 
-<?php
+    <?php
 }
 
 
 function elem()
 {
     global $category, $name, $con, $id, $prod;
-?>
-    <div class="form-group col-md-3 px-2" id="<?= $prod . "_{$id}" ?>">
-        <span class="fa fa-remove text-danger" style="position: absolute; margin: 4px 0px 0px -13px;" onclick="$(this).parent().remove()"></span>
-        <select name="<?= "{$category}_select[{$name}][]" ?>" id="<?= "{$category}_select_{$id}" ?>" class="border-0 small" style="width: 100%;">
+    ?>
+    <div class="form-group col-md-3 mx-2" id="<?= $prod . "_{$id}" ?>">
+        <span class="fa fa-remove text-danger" style="position: absolute; margin: 4px 0px 0px -13px; cursor: pointer;"
+            onclick="$(this).parent().remove()"></span>
+        <select name="<?= "{$category}_select[{$name}][]" ?>" id="<?= "{$category}_select_{$id}" ?>" class="border-0 small"
+            style="width: 100%;">
             <?php
             $Query = mysqli_query($con, "SELECT * FROM `product_list` WHERE `default_int` = '{$prod}'");
             while ($row = mysqli_fetch_assoc($Query)) {
                 $tbl_ui = mysqli_fetch_assoc(mysqli_query($con, "SELECT * FROM `tbl_ui_options` WHERE `sec_category_name` = '{$row['sec_category']}'"));
                 $input_box = ($tbl_ui['input_num'] != "True") ? false : true;
+                $prodId = $row['id'];
+
                 if ($tbl_ui['select_box'] != "True") {
                     echo "
                     <option value=''>{$row['default_name']}</option>
-                ";  
+                ";
                     break;
                 } else {
-                ?>
+                    ?>
                     <option value="<?= $row['prod_int'] ?>">
                         <?= $row['product'] ?>
-                    </option>   
-                <?php
+                    </option>
+                    <?php
                 }
             }
             ?>
         </select>
+        <div class="row">
 
-        <input type='number'  aria-describedby="PeriodUnit_<?= $id ?>" class='form-control small' id='<?= '{$category}_qty_{$id}' ?>' <?= (!$input_box) ? "disabled" : '' ?> min=0 placeholder='Quantity' name='<?= "{$category}_qty[{$name}][]" ?>'>
-        <span class="input-group-text form-control col-4 bg-light" id="<?= "{$prod}unit_{$id}" ?>">
-        <?php
-            if(count(getUnit($row[$id]))>1){
-                echo getUnit($row[$id])[0]["unit"];
-            }else{
-                ?>
-                <select name="<?= "{$category}_unit[{$name}][]" ?>" id="<?= "{$category}_unit_{$id}" ?>">
+            <input type='number' aria-describedby="<?= "{$prod}unit_{$id}" ?>" class='form-control small col-md-8'
+                id='<?= '{$category}_qty_{$id}' ?>' <?= (!$input_box) ? "disabled" : '' ?> min=0 placeholder='Quantity'
+                name='<?= "{$category}_qty[{$name}][]" ?>'>
+            <span class="input-group-text form-control col-4 bg-light p-0" id="<?= "{$prod}unit_{$id}" ?>">
                 <?php
-                    foreach (getUnit($row[$id]) as $arr){
-                        echo "<option value = '{$arr['id']}'>{$arr['unit_name']}<option>";
-                    }
+                    // PPrint((getUnit($prodId)));
+                if (count(getUnit($prodId)) == 1) {
+                    echo getUnit($prodId)[0]["unit_name"];
+                } else {
+                    ?>
+                    <select name="<?= "{$category}_unit[{$name}][]" ?>" id="<?= "{$category}_unit_{$id}" ?>"
+                        style="width : 100%; background: transparent;" class="form-control border-0 ">
+                        <?php
+                        foreach (getUnit($prodId) as $arr) {
+                            // PPrint($arr);
+                            echo "<option value = '{$arr['id']}'>{$arr['unit_name']}</option>";
+                        }
+                        ?>
+                    </select>
+                    <?php
+                }
                 ?>
-                </select>
-                <?php
-            }
-        ?>    
-    </span>
+            </span>
+        </div>
 
     </div>
-<?php
+    <?php
 }
 ?>
